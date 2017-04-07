40f81a3dc517b9d4c901172345cd96b9021d60bd
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-04-07 12:17:46 +0300

    first files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Clock from './App';
+import './index.css';
+
+ReactDOM.render(
+    <Clock/>,
+    document.getElementById('root')
+);
